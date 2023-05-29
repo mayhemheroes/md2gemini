0b7161f003d8eea32b32df7bd4d946bee3b14650
McKenna <dallmeyerm@att.net>
2023-05-28 20:53:16 -0400

    Changed to string

--- mayhem/fuzz_md.py
+++ mayhem/fuzz_md.py
@@ -7,18 +7,22 @@ import fuzz_helpers
 
 with atheris.instrument_imports():
     from md2gemini import md2gemini
+
 ctr = 0
 def TestOneInput(data):
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
     global ctr
     ctr += 1
-    with fdp.ConsumeTemporaryFile(suffix='.md', all_data=False, as_bytes=True) as f:
-        md2gemini(f, code_tag=fdp.ConsumeRandomString(), img_tag=fdp.ConsumeRandomString(), indent=fdp.ConsumeRandomString(),
+    try:
+        md2gemini(fdp.ConsumeRandomString(), code_tag=fdp.ConsumeRandomString(), img_tag=fdp.ConsumeRandomString(), indent=fdp.ConsumeRandomString(),
               ascii_table=fdp.ConsumeBool(), frontmatter=fdp.ConsumeBool(), jekyll=fdp.ConsumeBool(),
               links=fdp.ConsumeRandomString(), plain=fdp.ConsumeBool(), strip_html=fdp.ConsumeBool(),
               base_url=fdp.ConsumeRandomString(), md_links=fdp.ConsumeBool(), link_func=fdp.ConsumeRandomString(),
               table_tag=fdp.ConsumeRandomString(), checklist=fdp.ConsumeBool())
-
+    except IndexError:
+        if ctr > 1000:
+            raise
+        return -1
 
 def main():
     atheris.Setup(sys.argv, TestOneInput)

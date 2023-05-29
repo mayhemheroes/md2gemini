063e7f5447427a65c88c085773d3adbca185ec68
McKenna <dallmeyerm@att.net>
2023-05-28 21:10:12 -0400

    Error added in

--- mayhem/fuzz_md.py
+++ mayhem/fuzz_md.py
@@ -11,18 +11,12 @@ with atheris.instrument_imports():
 ctr = 0
 def TestOneInput(data):
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
-    global ctr
-    ctr += 1
-    try:
-        md2gemini(fdp.ConsumeRandomString(), code_tag=fdp.ConsumeRandomString(), img_tag=fdp.ConsumeRandomString(), indent=fdp.ConsumeRandomString(),
+
+    md2gemini(fdp.ConsumeRandomString(), code_tag=fdp.ConsumeRandomString(), img_tag=fdp.ConsumeRandomString(), indent=fdp.ConsumeRandomString(),
               ascii_table=fdp.ConsumeBool(), frontmatter=fdp.ConsumeBool(), jekyll=fdp.ConsumeBool(),
               links=fdp.ConsumeRandomString(), plain=fdp.ConsumeBool(), strip_html=fdp.ConsumeBool(),
               base_url=fdp.ConsumeRandomString(), md_links=fdp.ConsumeBool(), link_func=fdp.ConsumeRandomString(),
               table_tag=fdp.ConsumeRandomString(), checklist=fdp.ConsumeBool())
-    except IndexError:
-        if ctr > 100:
-            raise
-        return -1
 
 def main():
     atheris.Setup(sys.argv, TestOneInput)
